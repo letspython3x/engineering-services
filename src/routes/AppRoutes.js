15467eb4e5f7dd073e9c61706844d4edab851f86
abhishake <abhishake.gupta@rackspace.com>
2021-05-01 22:10:15 +0530

    Dynamically create routes from ServicesData

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,20 +1,24 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import AppConfig from '../config'
-import { Switch, Route } from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 import Home from '../components/Home'
 import About from '../components/About'
 import Contact from '../components/Contact'
-import Services from '../components/Services'
 import Dashboard from '../components/Dashboard'
+import {createServiceRoutes, getCategories} from '../utils';
+import {ServicesContext} from "../App";
 
 
 export default function AppRoutes() {
+    const servicesData = useContext(ServicesContext);
+    let servicesKeys = getCategories(servicesData)
+
     return (
         <Switch>
-            <Route path={AppConfig.homePageUrl} component={Home} />
-            <Route path={AppConfig.aboutPageUrl} component={About} />
-            <Route path={AppConfig.contactPageUrl} component={Contact} />
-            <Route path={AppConfig.servicesPageUrl} component={Services} />
-            <Route path={AppConfig.dashboardPageUrl} component={Dashboard} />
+            <Route path={AppConfig.homePageUrl} component={Home}/>
+            <Route path={AppConfig.aboutPageUrl} component={About}/>
+            <Route path={AppConfig.contactPageUrl} component={Contact}/>
+            <Route path={AppConfig.dashboardPageUrl} component={Dashboard}/>
+            {createServiceRoutes(servicesKeys)}
         </Switch>)
 }
