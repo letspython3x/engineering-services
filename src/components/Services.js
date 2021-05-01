7b5aa130e6ea08121af712cda9b53c582f85086b
abhishake <abhishake.gupta@rackspace.com>
2021-05-01 22:33:57 +0530

    Update Services Page to show by category-subCategory

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,30 +1,34 @@
-import React from 'react'
-import { Row, Col, Card } from 'antd'
-import servicesData from '../data/servicesData.json'
-// import '../static/css/servicesCard.css';
+import React, {useContext} from 'react'
+import {Card, Col, Row} from 'antd'
+import {ServicesContext} from "../App";
 
-const { Meta } = Card;
+const {Meta} = Card;
 
-function Services() {
-    const cardStyle = { width: 300, background: '#DDDDDD', padding: '2px 16px' };
-    const roundCorners = { borderRadius: '5px 5px 0 0' }
-    const data = servicesData;
+function Services(props) {
+    const cardStyle = {width: 300, background: '#DDDDDD', padding: '2px 16px'};
+    const roundCorners = {borderRadius: '5px 5px 0 0'}
+
+    const {category, subCategory} = props
+    const servicesData = useContext(ServicesContext);
+    let services = servicesData.filter(item => {
+        return (item.category === category && item.subCategory === subCategory)
+    })
 
     return (
-        <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-            {data.map((datum) => <Col>
+        <Row gutter={{xs: 8, sm: 16, md: 24, lg: 32}}>
+            {services.map((datum) => <Col>
                 <Card hoverable
-                    key={datum.id}
-                    style={{ ...cardStyle, ...roundCorners }}
-                    cover={
-                        <img style={{ ...roundCorners }}
-                            alt={datum.title}
-                            src={`images/services/${datum.imageName}`} />}>
-                    <Meta title={datum.title} description={datum.description} />
+                      key={datum.id}
+                      style={{...cardStyle, ...roundCorners}}
+                      cover={
+                          <img style={{...roundCorners}}
+                               alt={datum.title}
+                               src={`/images/services/${datum.imageName}`}/>}>
+                    <Meta title={datum.title} description={datum.description}/>
                 </Card>
             </Col>)
             }
-        </Row >
+        </Row>
     )
 }
 
